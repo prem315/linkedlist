8bd4f5acb5b6522dc86bd8fa9c3ac7188bd8b6a6
prem315 <prem.patel3105@gmail.com>
2021-05-29 21:10:58 +0530

    delete node in linkedlist

--- linkedList/index.js
+++ linkedList/index.js
@@ -42,13 +42,64 @@ LinkedList.prototype.search = function(data) {
     return false
 }
 
+LinkedList.prototype.deleteAtHead = function() {
+    if(this.isEmpty()) {
+        return false
+    }
+    let firstNode = this.head;
+    this.head = firstNode.nextEle
+    return this
+}
+
+LinkedList.prototype.deleteByValue = function(val) {
+    if(this.isEmpty()) {
+        return false
+    }
+    let currentNode = this.head
+    if(currentNode.data === val) {
+        this.head = currentNode.nextEle
+        return true
+    }
+    while(currentNode.nextEle !== null) {
+        if(currentNode.nextEle.data === val) {
+            currentNode.nextEle = currentNode.nextEle.nextEle
+            return true
+        }
+        currentNode = currentNode.nextEle 
+    }
+    return false;
+}
+
+LinkedList.prototype.deleteAtTail = function() {
+    if(this.isEmpty()) {
+        return false;
+    }
+    if(this.head.nextEle === null) {
+        this.head = null
+        return this
+    }
+    let currentNode = this.head
+    while(currentNode.nextEle !== null) {
+        console.log(currentNode)
+        if(currentNode.nextEle.nextEle === null) {
+            currentNode.nextEle = null
+            return this;
+        }
+        currentNode = currentNode.nextEle
+    }
+    return false
+}
+
 let list = new LinkedList();
-list.insertAtHead("1");
 list.insertAtHead("2");
+list.insertAtHead("1");
 list.insertAtTail("3");
 list.insertAtTail("6");
 list.insertAtTail("5");
 list.insertAtTail("10");
 console.log(list.search("10"))
+
+list.deleteAtTail();
+console.log("list", list);
 
 
