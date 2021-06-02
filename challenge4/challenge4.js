26a713395ae09451ea46c31359ea679af3d81286
prem315 <prem.patel3105@gmail.com>
2021-06-02 19:40:52 +0530

    find mid node using two pointers method

--- challenge4/challenge4.js
+++ challenge4/challenge4.js
@@ -17,6 +17,25 @@ function findMid(list) {
     }
 }
 
+function findMidUsingTwoPointers(list) {
+    console.log(list);
+    let slowPointer = list.head;
+    let fastPointer = list.head;
+
+    if(list.nextEle.nextEle === null) {
+        return list.nextEle;
+    } 
+
+    while(fastPointer !== null) {
+        slowPointer = slowPointer.nextEle;
+        fastPointer = fastPointer.nextEle.nextEle;
+    }
+
+    return slowPointer;
+}
+
+
+
 let list = new LinkedList();
 
 list.insertAtHead(21);
@@ -24,7 +43,7 @@ list.insertAtHead(14);
 list.insertAtHead(11);
 list.insertAtHead(7);
 list.insertAtTail(8);
-
+findMidUsingTwoPointers(list)
 
 
 findMid(list)
